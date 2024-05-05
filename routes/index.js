bc3a9ed7d5294ab90a0c4b2cd8541bb448535cca
tomarmayankk <mayanktomarpc@gmail.com>
2024-05-05 19:01:16 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+module.exports = router;
