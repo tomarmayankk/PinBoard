6c8ecb09974f6b97098c70d39a48226c07b01862
tomarmayankk <mayanktomarpc@gmail.com>
2024-05-06 22:57:33 +0530

    added some code to existing file

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,53 @@
 var express = require('express');
 var router = express.Router();
+const usermodel = require('./users');
+const passport = require('passport');
+const localStrategy = require('passport-local');
+
+passport.use(new localStrategy(usermodel.authenticate()));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+
 router.get('/register', function(req, res, next) {
   res.render('register');
 });
+
+router.get('/profile', isloggedin, function(req, res, next) {
+  res.render('profile');
+});
+
+router.post('/register', function(req, res, next) {
+  const data = new usermodel({
+    username: req.body.username,
+    email: req.body.email,
+    contact: req.body.contact
+  });
+  usermodel.register(data, req.body.password)
+    .then(function() {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/profile");
+      });
+    })
+});
+
+router.post('/login', passport.authenticate('local', {
+  failureRedirect: "/",
+  successRedirect: "/profile"
+}));
+
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+function isloggedin(req, res, next) {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+}
 
 module.exports = router;
